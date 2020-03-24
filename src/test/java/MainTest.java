a6b4572dcafe22febaf9a462ca89aa5e9e3ae5ce
ferran1 <ferrantl@live.nl>
2020-03-25 00:38:01 +0100

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Test class to test all the methods in Main.java
  * @author ferran
  */
 public class MainTest {
